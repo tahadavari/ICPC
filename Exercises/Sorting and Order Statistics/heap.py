dc188f618eed631dea7577fb5d4567816d8ddd04
Taha <tahadavari1381@gmail.com>
2021-10-10 11:08:22 +0330

    heap

---
+++ Exercises/Sorting and Order Statistics/heap.py
@@ -0,0 +1,17 @@
+# link:https://www.geeksforgeeks.org/heap-queue-or-heapq-in-python/
+
+from heapq import *
+
+li = [4,1,5,2,8,3]
+
+heapify(li)
+
+print(li)
+
+heappush(li,6)
+
+print(li)
+
+heappop(li)
+
+print(li)
