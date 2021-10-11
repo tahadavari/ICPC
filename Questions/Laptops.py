5b238997b8dea1cb4f964ec7a786e330ccf7b3ea
Taha <tahadavari1381@gmail.com>
2021-10-11 20:28:14 +0330

    sorting :: Laptops

---
+++ Questions/Laptops.py
@@ -0,0 +1,14 @@
+# link : https://codeforces.com/problemset/problem/456/A
+# sorting
+
+n = int(input())
+
+result = False
+l = []
+for i in range(n):
+    l.append(tuple(map(int,input().split())))
+
+l.sort(key=lambda x:x[0])
+
+print('Happy Alex') if all(True if l[i][1]>l[i+1][1] else False for i in range(len(l)-1) ) else print('Poor Alex')
+
