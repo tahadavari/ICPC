f529743007ec8fb4ed1d8befe361e90704ed3334
Taha <tahadavari1381@gmail.com>
2021-10-11 21:44:22 +0330

    sorting :: Laptops :: accept

--- Questions/Laptops.py
+++ Questions/Laptops.py
@@ -9,6 +9,7 @@ for i in range(n):
     l.append(tuple(map(int,input().split())))
 
 l.sort(key=lambda x:x[0])
+# print(l)
 
-print('Happy Alex') if all(True if l[i][1]>l[i+1][1] else False for i in range(len(l)-1) ) else print('Poor Alex')
+print('Happy Alex') if any([True if l[i][1]>l[i+1][1] else False for i in range(len(l)-1)]) else print('Poor Alex')
 
