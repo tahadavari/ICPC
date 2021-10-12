d60fef3e92aacbb9d00bcefb33b3d9feae3c996a
Taha <tahadavari1381@gmail.com>
2021-10-12 09:29:26 +0330

    sorting :: Reading :: accept with file I/O

--- Questions/Reading.py
+++ Questions/Reading.py
@@ -1,16 +1,28 @@
 # link : https://codeforces.com/problemset/problem/234/B
 # sorting
 
-n, k = map(int, input().split())
+f = ''
+with open('input.txt', 'r') as file:
+    for i,line in enumerate(file.readlines()):
+        if i == 0:
+            n, k = map(int,line.split())
+        else:
+            l = {str(index): int(value) for index, value in enumerate(line.split())}
 
-l = {str(index): int(value) for index, value in enumerate(input().split())}
 
 l = dict(sorted(l.items(), key=lambda x: x[1], reverse=True))
 
-# print(l)
-for i,key in enumerate(l.keys()):
-    if i<k:
-        print(int(key)+1,end=' ')
-    else:
-        break
-
+with open('output.txt','w') as file:
+    r = []
+    min = 1000
+    for i, item in enumerate(l.items()):
+        if i < k:
+            r.append(int(item[0])+1)
+            if item[1]<min:
+                min=item[1]
+        else:
+            break
+    r.sort()
+    r=list(map(str,r))
+    file.write(str(min)+'\n')
+    file.write(' '.join(r))
